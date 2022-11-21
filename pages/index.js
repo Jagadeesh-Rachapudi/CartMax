07b5df6040028d6f0aa55820fa8c59bc0b933745
JAGADEESH RACHAPUDI <rachapudijagadeesh9580@gmail.com>
2022-11-21 06:25:01 +0530

    Updated

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <form action="/HF">
-        <input type="submit" value="Go to Page1" className="Basic-Buttons1" />
-      </form>
-      <form action="/FA">
-        <input type="submit" value="Go to Page2" className="Basic-Buttons2" />
-      </form>
-      <form action="/E">
-        <input type="submit" value="Go to Page3" className="Basic-Buttons3" />
-      </form>
+      <div className="Pages">
+      <a href="/HF">Page1</a>
+      <a href="/FA">Page2</a>
+      <a href="/E">Page3</a>
+      </div>
     </div>
   );
 }
