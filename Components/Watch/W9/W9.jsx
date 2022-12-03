cb4da11f46530d8ce8a54408ab97b4c43614c99d
Jagadeesh <rachapudijagadeesh9580@gmail.com>
2022-12-03 17:30:44 +0530

    Updated

--- Components/Watch/W9/W9.jsx
+++ Components/Watch/W9/W9.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function W9() {
   return (
-    <div className='W9-body' >W9</div>
-  )
+    <div className="W9-body">
+      <div className="p1">
+        <div className="tag">NEW ARRIVALS 2022</div>
+        <div className="title">
+          LUXURY PERFUME <br /> 2022
+        </div>
+        <div className="discover">Discover Now</div>
+      </div>
+      <div className="p2">
+        <div className="tag">TRENDING PRODUCTS 2022</div>
+        <div className="title t2">
+          MAURICE LACROIX <br /> WATCH
+        </div>
+        <div className="discover">Discover Now</div>
+      </div>
+    </div>
+  );
 }
 
-export default W9
+export default W9;
